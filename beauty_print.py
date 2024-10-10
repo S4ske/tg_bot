5d3b72804357e59cc111fd1088a86a0a15e9984b
S4ske <sany1111.ru@mail.ru>
2024-10-10 18:19:30 +0500

    Зарефакторил немножко

--- beauty_print.py
+++ beauty_print.py
@@ -1,28 +1,29 @@
-def print_CalendarTask(CalendarTask):
-    return (f'Задание <b>№{CalendarTask.id}</b>\n'
-          f"Дата: <u>{CalendarTask.calendarDate[:10]}</u>\n"
-          f'Продолжительность: {CalendarTask.calendarDuration}ч\n'
-          f'Время начала работы: <u>{CalendarTask.calendarDate[:10]}</u>\n'
-          f'Наименование техники: {CalendarTask.machine}\n')
+def print_calendar_task(calendar_task):
+    return (f'Задание <b>№{calendar_task.id}</b>\n'
+            f"Дата: <u>{calendar_task.calendarDate[:10]}</u>\n"
+            f'Продолжительность: {calendar_task.calendarDuration}ч\n'
+            f'Время начала работы: <u>{calendar_task.calendarDate[:10]}</u>\n'
+            f'Наименование техники: {calendar_task.machine}\n')
 
 
-def print_Waybill(Waybill):
-    return (f'Путевой лист <b>№{Waybill.id}</b>\n'
-            f'Дата: <u>{Waybill.date[:10]}</u>\n'
-            f'Продолжительность: {Waybill.duration}ч\n'
-            f'Гос.номер: {Waybill.govNumber}\n'
-            f'Значение одометра в конце: {Waybill.mileageEnd}\n'
-            f'Значение одометра на старте: {Waybill.mileageStart}\n'
-            f'Тип техники: {Waybill.typeM}')
+def print_waybill(waybill):
+    return (f'Путевой лист <b>№{waybill.id}</b>\n'
+            f'Дата: <u>{waybill.date[:10]}</u>\n'
+            f'Продолжительность: {waybill.duration}ч\n'
+            f'Гос.номер: {waybill.govNumber}\n'
+            f'Значение одометра в конце: {waybill.mileageEnd}\n'
+            f'Значение одометра на старте: {waybill.mileageStart}\n'
+            f'Тип техники: {waybill.typeM}')
 
-def print_Task(Task):
-    return(f'Задание <b>№{Task.id}</b>\n'
-           f'Заказчик: {Task.order}\n'
-           f'Время прибытия: <u>{Task.arrivalTime}</u>\n'
-           f'Адрес погрузки: {Task.loadingAddress}\n'
-           f'Адресс разгрузки: {Task.unloadingAddress}\n'
-           f'Наименование груза: {Task.cargoName}\n'
-           f'Количество ездок: {Task.count}\n'
-           f'Расстояние: {Task.distance} км\n'
-           f'Перевезти: {Task.transported} тонн\n'
-           f'Примечание {Task.description}')
+
+def print_task(task):
+    return (f'Задание <b>№{task.id}</b>\n'
+            f'Заказчик: {task.order}\n'
+            f'Время прибытия: <u>{task.arrivalTime}</u>\n'
+            f'Адрес погрузки: {task.loadingAddress}\n'
+            f'Адресс разгрузки: {task.unloadingAddress}\n'
+            f'Наименование груза: {task.cargoName}\n'
+            f'Количество ездок: {task.count}\n'
+            f'Расстояние: {task.distance} км\n'
+            f'Перевезти: {task.transported} тонн\n'
+            f'Примечание {task.description}')
